5ca6c226358fbc54053262f28ec040e7de5e30b5
Dejota Freitas <djxpgs@hotmail.com>
2019-09-13 10:01:17 -0300

    s

---
+++ CRUD_SQLITE.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+
+
+namespace CRUD_SQLITE {
+
+
+    public partial class Form1 : Form
+    {
+        // NuGet install: System.Data.SQLite
+        static string data_source = @"Data Source=DEJOTADB.db;Version=3;New=True;Compress=True;UTF8Encoding=True;";
+        static SQLiteConnection conexao;
+        static SQLiteCommand comandos;
+
+        public Form1()
+        {
+            Console.WriteLine("=================SQLITE=====================");
+            conexao = new SQLiteConnection(data_source);
+            conexao.Open();
+            comandos = conexao.CreateCommand();
+            comandos.CommandText = "CREATE TABLE IF NOT EXISTS " +
+                "cidade(id integer primary key, nome VARCHAR(40));";
+            comandos.ExecuteNonQuery();
+; ;
+            InitializeComponent();
+        }
+
+    
+
+        private void Btn_ler_Click(object sender, EventArgs e)
+        {
+            SQLiteDataReader leitor;
+            comandos.CommandText = "SELECT * FROM cidade;";
+            leitor = comandos.ExecuteReader();
+            while (leitor.Read())
+            {
+                int idx = leitor.GetInt32(0);
+                string cidade = leitor.GetString(1);
+                Console.WriteLine("ID: "+ idx + ", CIDADA: "+ cidade);
+            }
+            leitor.Close();
+            Console.WriteLine("=================AND=====================");
+        }
+
+        private void Btn_inserir_Click(object sender, EventArgs e)
+        {
+            comandos.CommandText = "INSERT INTO cidade values(null, 'Limoeiro do Norte');";
+            comandos.ExecuteNonQuery();
+        }
+
+        private void Btn_alterar_Click(object sender, EventArgs e)
+        {
+            comandos.CommandText = "UPDATE cidade SET nome = 'Alegre';";
+            comandos.ExecuteNonQuery();
+        }
+
+        private void Btn_apagar_Click(object sender, EventArgs e)
+        {
+            comandos.CommandText = "DELETE FROM cidade;";
+            comandos.ExecuteNonQuery();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //fechar conexao ao sair do app
+            conexao.Close();
+        }
+    }
+}
---
+++ SCREENSHOT_FULL_WEB_PAGE.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using iTextSharp.text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using WDSE;
+using WDSE.Decorators;
+using WDSE.ScreenshotMaker;
+
+// Nuget install: selenium e driver dos browser ultilizado
+// Nuget install: Noksa = percorre a tela tirando prints e salva array de bytes
+// Nuget install: iTextSharp = converter img pdf
+namespace Screenshot_Capture
+{
+    public partial class Form1 : Form
+    {
+        private IWebDriver driver;
+        private String url;
+        private String path;
+        private FolderBrowserDialog folderBrowserDialog;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.url = "https://www.mercadopago.com.br/";
+            this.path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+        }
+
+        private void capiturar_screenshot(object sender, EventArgs e)
+        {
+            if (this.driver != null) {
+                try
+                {
+                    //ScreenshotImageFormat format;
+                    //JPG
+                    if (rb_formato_jpg.Checked == true)
+                    {     
+                        //format = ScreenshotImageFormat.Jpeg;
+                        String filePathName = path + "\\" + "Screenshot_Capture_" + DateTime.Now.ToString("yyyy_MM_dd_hh_mm_ss") + ".jpeg";
+                        //((ITakesScreenshot)driver).GetScreenshot().SaveAsFile(filePathName, format);
+
+                        var bytesArray = driver.TakeScreenshot(new VerticalCombineDecorator(new ScreenshotMaker().RemoveScrollBarsWhileShooting()));
+                        MemoryStream ms = new MemoryStream(bytesArray, 0, bytesArray.Length);
+                        System.Drawing.Image image = System.Drawing.Image.FromStream(ms, true);
+                        image.Save(filePathName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                        bytesArray = null;
+                        image.Dispose();
+
+                        //PNG
+                    } else if (rb_formato_png.Checked == true) {
+                        //format = ScreenshotImageFormat.Png;
+                        String filePathName = path + "\\" + "Screenshot_Capture_" + DateTime.Now.ToString("yyyy_MM_dd_hh_mm_ss") + ".png";
+                        //((ITakesScreenshot)driver).GetScreenshot().SaveAsFile(filePathName, format)
+
+                        var bytesArray = driver.TakeScreenshot(new VerticalCombineDecorator(new ScreenshotMaker().RemoveScrollBarsWhileShooting()));
+                        MemoryStream ms = new MemoryStream(bytesArray, 0, bytesArray.Length);
+                        System.Drawing.Image image = System.Drawing.Image.FromStream(ms, true);
+                        image.Save(filePathName, System.Drawing.Imaging.ImageFormat.Png);
+                        bytesArray = null;
+                        image.Dispose();
+
+                        //PDF
+                    } else if (rb_formato_pdf.Checked == true){
+                        //format = ScreenshotImageFormat.Jpeg;
+
+                        String filePath = path;
+                        String fileName = "Screenshot_Capture_" + DateTime.Now.ToString("yyyy_MM_dd_hh_mm_ss");
+                        String filePathName = filePath + "\\" + fileName;
+                        //((ITakesScreenshot)driver).GetScreenshot().SaveAsFile(filePathName+ ".jpeg", format);
+
+                        var bytesArray = driver.TakeScreenshot(new VerticalCombineDecorator(new ScreenshotMaker().RemoveScrollBarsWhileShooting()));
+                        MemoryStream ms = new MemoryStream(bytesArray, 0, bytesArray.Length);
+                        System.Drawing.Image image = System.Drawing.Image.FromStream(ms, true);
+                        image.Save(filePathName + ".jpeg", System.Drawing.Imaging.ImageFormat.Jpeg);
+                        bytesArray = null;
+                        image.Dispose();
+
+                        //criando pdf
+                        Document doc = new Document();
+                        try {
+                            iTextSharp.text.pdf.PdfWriter.GetInstance(doc, new FileStream(filePathName + ".pdf", FileMode.Create));
+                            doc.Open();
+                            iTextSharp.text.Image jpg = iTextSharp.text.Image.GetInstance(filePathName + ".jpeg");
+                            jpg.Border = iTextSharp.text.Rectangle.BOX;
+                            var jpeg = new Jpeg(jpg);
+                            jpeg.ScaleToFit(doc.PageSize.Width - (doc.LeftMargin + doc.RightMargin),
+                              doc.PageSize.Height - (doc.BottomMargin + doc.TopMargin));
+                            doc.Add(jpeg);                    
+                        }
+                        catch (Exception ex) {
+                            Console.WriteLine(ex);
+                            MessageBox.Show("Tivemos algum problema ao salvar em PDF", "Mensagem");
+                        } finally {
+                            doc.Close();
+                            File.Delete(filePathName + ".jpeg");
+                        }
+                    }   
+                
+                    System.Diagnostics.Process.Start("Explorer", path);
+
+                    /*
+                    var result = MessageBox.Show("Imagem salva com sucesso, " +
+                        "deseja abrir diretorio onde a screenshot esta salva ?", 
+                        "Mensagem",
+                        MessageBoxButtons.YesNo,
+                        MessageBoxIcon.Question);
+                    if (result == DialogResult.Yes)  {
+                        System.Diagnostics.Process.Start("Explorer", path);
+                    }
+                    */
+
+                } catch (Exception ex) {
+                    Console.WriteLine(ex);
+                    MessageBox.Show("Tivemos algum problema ao salvar a screenshot, " +
+                        "provavelmente o navegar foi fechado ou a sua primeira aba, " +
+                        "onde seria realizada a screenshot.", "Mensagem");
+                    if (this.driver != null) { this.driver.Quit(); }
+                }
+            } else {
+                MessageBox.Show("Abra o navegar através do nosso aplicativo " +
+                    "antes de tentar capiturar a screenshot.", "Mensagem");
+            }
+
+        }
+
+        private void execute_navegador(object sender, EventArgs e)
+        {
+            try
+            {
+                //Chrome
+                if (rb_navegador_chorme.Checked == true) {
+                    var driverService = ChromeDriverService.CreateDefaultService();
+                    driverService.HideCommandPromptWindow = true;
+                    ChromeOptions options = new ChromeOptions();
+                    options.AddArgument("--start-maximized");
+                    driver = new ChromeDriver(driverService, options);
+                    driver.Url = url;
+                }
+                // Firefox
+                else if(rb_navegador_firefox.Checked == true) {
+                    var driverService = FirefoxDriverService.CreateDefaultService();
+                    driverService.HideCommandPromptWindow = true;
+                    FirefoxOptions options = new FirefoxOptions();
+                    options.AddArgument("--start-maximized");
+                    driver = new FirefoxDriver(driverService, options);
+                    //driver.Url = url;
+                    driver.Navigate().GoToUrl(url);
+                } else {
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Tivemos algum erro ao tentar abrir o navegador de internet", "Mensagem");
+                Console.WriteLine(ex);
+            }
+        }
+
+       
+        // selecionar path salvar imagens
+        private void path_select(object sender, EventArgs e)
+        {
+            try
+            {
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    String caminho = folderBrowserDialog.SelectedPath;
+                    tb_path.Text = caminho;
+                    this.path = caminho;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                MessageBox.Show("Tivemos algum problema ao tentar definir esse diretório.", "Mensagem");
+            }            
+            
+        }
+
+        // ao carrgar aplicação
+        private void janela_principal_load(object sender, EventArgs e)
+        {
+            try
+            {
+                tb_path.Text = this.path;
+                tb_url.Text = this.url;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                MessageBox.Show("Tivemos algum problema ao abrir a aplicação.", "Mensagem");
+            }
+        }
+
+        // ao fechar aplicação
+        private void janela_principal_closed(object sender, FormClosedEventArgs e)
+        {
+            try
+            {
+                if (this.driver != null) { this.driver.Quit(); }
+            }
+            catch (Exception ex) { Console.WriteLine(ex); }
+        }
+
+        private void salvar_url(object sender, EventArgs e)
+        {
+            try
+            {
+                this.url = tb_url.Text;
+                MessageBox.Show("URL salva com sucesso.", "Mensagem");
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex);
+                MessageBox.Show("Tivemos algum problema ao salvar a URL.", "Mensagem");
+            }
+        }
+
+
+    }// fim classe
+}
